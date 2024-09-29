570d053f2173113ede6a6901a5f580fc4a7ac6a0
stockimen <stockimen@qq.com>
2024-09-29 20:26:24 +0800

    Update template.js

--- src/template.js
+++ src/template.js
@@ -11,7 +11,7 @@ const SWITCHER = (text, open, className = '') => `
   <span class="slider round"></span>
 </label>
 `
-const FOOTER = ({ lang, isEdit, updateAt, pw, mode, share, save, autoSave }) => `
+const FOOTER = ({ lang, isEdit, pw, mode, share, save, autoSave }) => `
     <div class="footer">
         ${isEdit ? `
             <div class="opt">
@@ -29,7 +29,7 @@ const FOOTER = ({ lang, isEdit, updateAt, pw, mode, share, save, autoSave }) => 
         <a class="github-link" title="Github" target="_blank" href="https://github.com/s0urcelab/serverless-cloud-notepad" rel="noreferrer">
             <svg viewBox="64 64 896 896" focusable="false" data-icon="github" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
         </a>
-        ${updateAt ? `<span class="last-modified">${SUPPORTED_LANG[lang].lastModified} ${dayjs.unix(updateAt).fromNow()}</span>` : ''}
+        
     </div>
 `
 const MODAL = lang => `
@@ -44,7 +44,7 @@ const MODAL = lang => `
     </div>
 </div>
 `
-const HTML = ({ lang, title, content, ext = {}, tips, isEdit, showPwPrompt }) => `
+const HTML = ({ lang, title, updateAt, content, ext = {}, tips, isEdit, showPwPrompt }) => `
 <!DOCTYPE html>
 <html>
 <head>
@@ -54,12 +54,13 @@ const HTML = ({ lang, title, content, ext = {}, tips, isEdit, showPwPrompt }) =>
     <title>${title} — Cloud Notepad</title>
     <link href="${CDN_PREFIX}/favicon.ico" rel="shortcut icon" type="image/ico" />
     <!-- <link href="${CDN_PREFIX}/css/app.min.css" rel="stylesheet" media="screen" /> -->
-    <link href="${CDN_PREFIX}/css/app.css" rel="stylesheet" media="screen" />
+    <link href="${CDN_PREFIX}/css/app.min.css" rel="stylesheet" media="screen" />
 </head>
 <body>
     <div class="note-container">
         <div class="stack">
             <div class="layer_1">
+            ${updateAt ? `<span class="last-modified">${SUPPORTED_LANG[lang].lastModified} ${dayjs.unix(updateAt).fromNow()}</span>` : ''}
                 <div class="layer_2">
                     <div class="layer_3">
                         ${tips ? `<div class="tips">${tips}</div>` : ''}
@@ -78,7 +79,7 @@ const HTML = ({ lang, title, content, ext = {}, tips, isEdit, showPwPrompt }) =>
     ${ext.mode === 'md' ? `<script src="${CDN_PREFIX}/js/marked.min.js"></script>` : ''}
     <script src="${CDN_PREFIX}/js/clip.min.js"></script>
 <!--     <script src="${CDN_PREFIX}/js/app.min.js"></script>-->
-    <script src="${CDN_PREFIX}/js/app.js"></script>
+    <script src="${CDN_PREFIX}/js/app.min.js"></script>
     ${showPwPrompt ? '<script>passwdPrompt()</script>' : ''}
 </body>
 </html>
